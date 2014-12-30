10ea459bf5864d72ba70cb9d9442777661c13858
LeeannDrees <leeann.drees@gmail.com>
2014-12-30 12:14:40 -0500

    initial commit

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bLose Weight\b/g, "Get Pizza");
+	v = v.replace(/\bLose weight\b/g, "Get pizza");
+	v = v.replace(/\blose weight\b/g, "get pizza");
+	v = v.replace(/\bLOSE WEIGHT\b/g, "GET PIZZA");
 	
 	textNode.nodeValue = v;
 }
